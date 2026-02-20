d47cf478bf66a64b71aff5c559c7ba94adf1c1a3
ElvinIsmayil <ismayilelvin1@gmail.com>
2026-02-21 01:42:32 +0300

    fix: successfully resolved all merge conflicts

--- codedu/src/main/java/module-info.java
+++ codedu/src/main/java/module-info.java
@@ -1,27 +1,10 @@
-<<<<<<< HEAD
 module com.codedu {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens com.codedu to javafx.fxml;
+    opens com.codedu.views to javafx.fxml;
     opens com.codedu.controllers to javafx.fxml;
-    opens com.codedu.models to javafx.fxml;
 
     exports com.codedu;
-    exports com.codedu.controllers;
     exports com.codedu.models;
 }
-=======
-module com.codedu {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.codedu to javafx.fxml;
-    opens com.codedu.controllers to javafx.fxml;
-    opens com.codedu.models to javafx.fxml;
-
-    exports com.codedu;
-    exports com.codedu.controllers;
-    exports com.codedu.models;
-}
->>>>>>> 68084a056cecabbca83ad1f2bea89e41b0d5769a
